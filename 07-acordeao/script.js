25351aa460e60097ba4536b50c7d7e10f580abb2
brun0IF <bruno4ndr0d@gmail.com>
2025-04-22 19:38:32 -0300

    intAccordion

--- 07-acordeao/script.js
+++ 07-acordeao/script.js
@@ -7,5 +7,16 @@ function initTabNav(){
 initTabNav();
 
 function intAccordion() {
-  
+
+  const accordion = document.querySelectorAll('.js-accordion dt');
+
+  function accordionList(){
+    console.log('clicou no dt');
+  }
+
+
+  accordionList.forEach((item) =>{
+
+    item.addEventListener('click', activeAccordion);
+  })
 }
